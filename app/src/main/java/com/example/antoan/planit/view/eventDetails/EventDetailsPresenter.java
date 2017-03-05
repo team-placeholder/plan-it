8ed0edf9e40cd3ffe64a27460f46977091f8006e
pavelhristov <pavel.hristov_@abv.bg>
2017-03-05 18:39:20 +0200

    alarm for event

--- app/src/main/java/com/example/antoan/planit/view/eventDetails/EventDetailsPresenter.java
+++ app/src/main/java/com/example/antoan/planit/view/eventDetails/EventDetailsPresenter.java
@@ -1,11 +1,23 @@
 package com.example.antoan.planit.view.eventDetails;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 import com.data.EventsData;
 import com.data.models.EventResponse;
+import com.data.models.PlanedEvent;
+import com.example.antoan.planit.view.AlarmReceiverActivity;
+
+import java.util.Calendar;
 
 import javax.inject.Inject;
 
 import io.reactivex.functions.Consumer;
+
+import static android.content.Context.ALARM_SERVICE;
 
 /**
  * Created by hristov on 2.3.2017 г..
@@ -15,6 +27,7 @@ public class EventDetailsPresenter implements  EventDetailsContracts.Presenter {
     private final EventsData eventsData;
     private EventDetailsContracts.View view;
     private String eventId;
+    private PlanedEvent event;
 
     @Inject
     public EventDetailsPresenter(EventDetailsContracts.View view, EventsData eventsData) {
@@ -31,8 +44,10 @@ public class EventDetailsPresenter implements  EventDetailsContracts.Presenter {
     @Override
     public void start() {
         eventsData.getEventById(eventId).subscribe(new Consumer<EventResponse>() {
+
             @Override
             public void accept(EventResponse eventResponse) throws Exception {
+                event = eventResponse.getEvent();
                 getView().notify(eventResponse.getMessage());
                 getView().setEvent(eventResponse.getEvent());
             }
@@ -42,5 +57,38 @@ public class EventDetailsPresenter implements  EventDetailsContracts.Presenter {
     @Override
     public void setEventId(String id) {
         this.eventId = id;
+    }
+
+    @Override
+    public void setAlarm(Context ctx) {
+
+        int year = this.event.getDate().getYear();
+        int month = this.event.getDate().getMonth() - 1;
+        int day = this.event.getDate().getDay();
+
+        String[] times = this.event.getStart().split(":");
+        Integer hour  = Integer.valueOf(times[0]);
+        int minutes = Integer.valueOf(times[1]);
+
+        String message = this.event.getTitle();
+
+        Calendar cal=Calendar.getInstance();
+        cal.set(Calendar.YEAR,year);
+        cal.set(Calendar.MONTH,month);
+        cal.set(Calendar.DAY_OF_MONTH,day);
+        cal.set(Calendar.HOUR_OF_DAY,hour);
+        cal.set(Calendar.MINUTE,minutes);
+        cal.add(Calendar.SECOND,5);
+
+        Intent intent = new Intent(ctx, AlarmReceiverActivity.class);
+
+        intent.putExtra(AlarmReceiverActivity.MESSAGE_KEY, message);
+        PendingIntent pendingIntent = PendingIntent.getActivity(ctx,
+                12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) ctx.getSystemService(ALARM_SERVICE);
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),pendingIntent );
+
+        this.getView().notify("Alarm Set.");
     }
 }
--- app/src/main/java/com/example/antoan/planit/view/eventDetails/EventDetailsView.java
+++ app/src/main/java/com/example/antoan/planit/view/eventDetails/EventDetailsView.java
@@ -1,16 +1,19 @@
 package com.example.antoan.planit.view.eventDetails;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.data.models.PlanedEvent;
 import com.example.antoan.planit.R;
 
-public class EventDetailsView extends Fragment implements EventDetailsContracts.View {
+public class EventDetailsView extends Fragment implements EventDetailsContracts.View, View.OnClickListener {
 
 
     private EventDetailsContracts.Presenter presenter;
@@ -19,6 +22,8 @@ public class EventDetailsView extends Fragment implements EventDetailsContracts.
     private TextView tvEndTime;
     private TextView tvStartTime;
     private TextView tvDescription;
+    private Button btnAlarm;
+    private Context ctx;
 
     public EventDetailsView() {
 
@@ -29,12 +34,15 @@ public class EventDetailsView extends Fragment implements EventDetailsContracts.
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root =  inflater.inflate(R.layout.fragment_event_details_view, container, false);
+        this.ctx = root.getContext();
         this.tvDate = (TextView) root.findViewById(R.id.tv_date);
         this.tvTitle = (TextView) root.findViewById(R.id.tv_title);
         this.tvDescription = (TextView) root.findViewById(R.id.tv_description);
         this.tvStartTime = (TextView) root.findViewById(R.id.tv_start_time);
         this.tvEndTime = (TextView) root.findViewById(R.id.tv_end_time);
+        this.btnAlarm = (Button) root.findViewById(R.id.btn_alarm);
 
+        this.btnAlarm.setOnClickListener(this);
         this.presenter.start();
 
         return root;
@@ -47,7 +55,7 @@ public class EventDetailsView extends Fragment implements EventDetailsContracts.
 
     @Override
     public void notify(String message) {
-        this.tvDate.setText(message);
+        Toast.makeText(ctx, message, Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -57,5 +65,12 @@ public class EventDetailsView extends Fragment implements EventDetailsContracts.
         this.tvDescription.setText(event.getDescription());
         this.tvStartTime.setText(event.getStart());
         this.tvEndTime.setText(event.getEnd());
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        this.presenter.setAlarm(this.ctx);
+
     }
 }
